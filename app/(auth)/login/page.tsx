533a2c6a9be62ceeef8a914b27fca0c8e3ddb8b7
Ved <bbbbaisayan@gmail.com>
2026-03-05 13:51:43 +0530

    Added routes for auth api, and auth-utils for securing all routes

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginUI from "@/module/auth/login-ui";
+// import { requireUnAuth } from "@/module/auth/auth-utils";
+
+const LoginPage = () => {
+//   await requireUnAuth();
+
+  return <LoginUI />;
+};
+
+export default LoginPage;
